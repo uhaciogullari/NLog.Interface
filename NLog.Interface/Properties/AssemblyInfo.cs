020507a3da0254beaff3af1110ac49e6498b1bb6
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2013-01-01 20:49:24 +0200

    Version 1.0.0

--- NLog.Interface/Properties/AssemblyInfo.cs
+++ NLog.Interface/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NLog.Interface")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("yenibiris.com")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NLog.Interface")]
-[assembly: AssemblyCopyright("Copyright © yenibiris.com 2012")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
